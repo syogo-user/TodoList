16457a5dba8357aba15f9d5b813b554d17f66aed
syogo-user <s.photo.trumpet.005@gmail.com>
2021-09-27 11:29:05 +0900

    feat : リストビューの表示

--- app/src/main/java/com/example/todolist/TaskAdapter.kt
+++ app/src/main/java/com/example/todolist/TaskAdapter.kt
@@ -1,20 +1,20 @@
 package com.example.todolist
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import com.example.todolist.ui.list.ListFragment
 import java.text.SimpleDateFormat
 import java.util.*
 
-class TaskAdapter(context: Context) : BaseAdapter() {
+class TaskAdapter(fragment: ListFragment) : BaseAdapter() {
     private val mLayoutInflater: LayoutInflater
     var taskList = mutableListOf<Task>()
 
     init {
-        this.mLayoutInflater = LayoutInflater.from(context)
+        this.mLayoutInflater = LayoutInflater.from(fragment.context)
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
@@ -24,8 +24,8 @@ class TaskAdapter(context: Context) : BaseAdapter() {
         textView1.text = taskList[position].title
 
         var simpleDateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.JAPANESE)
-        val date = taskList[position].date
-        textView2.text = simpleDateFormat.format(date)
+//        val date = taskList[position].date
+//        textView2.text = simpleDateFormat.format(date)
         return view
     }
 
--- app/src/main/java/com/example/todolist/ui/list/ListFragment.kt
+++ app/src/main/java/com/example/todolist/ui/list/ListFragment.kt
@@ -8,11 +8,14 @@ import android.view.ViewGroup
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.example.todolist.R
+import com.example.todolist.Task
 import com.example.todolist.TaskAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ListFragment : Fragment() {
     private lateinit var mTaskAdapter: TaskAdapter
+    private lateinit var listView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,16 +32,68 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val listView = view.findViewById<ListView>(R.id.listView)
-
+        listView = view.findViewById<ListView>(R.id.listView)
         val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+        mTaskAdapter = TaskAdapter( this@ListFragment)
         fab.setOnClickListener {
             Log.d("TAG","hello!")
 //            val intent = Intent(this@ListActivity, InputActivity::class.java)
 //            intent.putExtra(EXTRA_TASK_ID, mTaskAdapter.getMaxId())
 //            startActivity(intent)
         }
+        // TODO ログインの確認後
+        reloadListView()
     }
+
+    private fun reloadListView() {
+        // データを取得し、日付順にソート
+        val db = FirebaseFirestore.getInstance()
+        val tasks = db.collection("tasks")
+        tasks.get()
+            .addOnSuccessListener { documents ->
+                val taskList = documents.toObjects(Task::class.java)
+                mTaskAdapter.taskList = taskList
+                // ListViewのアダプターに設定する
+                listView.adapter = mTaskAdapter
+                // アダプターにデータの変更を通知する
+                mTaskAdapter.notifyDataSetChanged()
+                for (document in documents) {
+
+                    Log.d("TAG", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("TAG", "Error getting documents: ", exception)
+            }
+
+
+
+
+
+//        tasks.get().addOnCompleteListener(OnCompleteListener { it ->
+//            if (it.isSuccessful()) {
+//                it.result?.let {
+//                    val taskList = it.toObjects(Task::class.java)
+//                    mTaskAdapter.taskList = taskList
+//                    // ListViewのアダプターに設定する
+//                    listView.adapter = mTaskAdapter
+//                    // アダプターにデータの変更を通知する
+//                    mTaskAdapter.notifyDataSetChanged()
+//                }
+//            }
+//        })
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 //    private lateinit var homeViewModel: HomeViewModel
 //    private var _binding: FragmentHomeBinding? = null
