c3c68ff9b2d316a44d8252181836d3cafa819c53
syogo-user <s.photo.trumpet.005@gmail.com>
2021-10-02 16:26:01 +0900

    feat: カレンダーの表示
    タスクを日付ごとに管理しており、カレンダーにより見やすくするため。

--- app/src/main/java/com/example/todolist/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/todolist/ui/calendar/CalendarFragment.kt
@@ -4,42 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import com.applandeo.materialcalendarview.CalendarView
+import com.example.todolist.R
 
 class CalendarFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view =inflater.inflate(R.layout.fragment_calendar, container, false)
+        return view
+    }
 
-//    private lateinit var notificationsViewModel: NotificationsViewModel
-//    private var _binding: FragmentNotificationsBinding? = null
-//
-//    // This property is only valid between onCreateView and
-//    // onDestroyView.
-//    private val binding get() = _binding!!
-//
-//    override fun onCreateView(
-//            inflater: LayoutInflater,
-//            container: ViewGroup?,
-//            savedInstanceState: Bundle?
-//    ): View? {
-//        notificationsViewModel =
-//                ViewModelProvider(this).get(NotificationsViewModel::class.java)
-//
-//        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-//        val root: View = binding.root
-//
-//        val textView: TextView = binding.textNotifications
-//        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
-//        return root
-//    }
-//
-//    override fun onDestroyView() {
-//        super.onDestroyView()
-//        _binding = null
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val calendarView = view.findViewById<CalendarView>(R.id.calendarView)
+    }
+
 }
